c95fe8c2865c5efa4592afbaf0bbd898f25da81b
Akash Barsagadey <Akash.Barsagadey@blueconchtech.com>
2023-03-14 11:59:42 +0530

    Completed module 2

--- src/app.js
+++ src/app.js
@@ -12,12 +12,57 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// create a GET route for the root URL path '/'
+// Read contents of accounts.json file
+const accountData = fs.readFileSync('src/json/accounts.json', { encoding: 'utf8' });
+
+// Parse JSON into JavaScript object
+const accounts = JSON.parse(accountData);
+
+
+// Read contents of users.json file
+const userData  = fs.readFileSync('src/json/users.json', { encoding: 'utf8' });
+
+// Parse JSON into JavaScript object
+const users  = JSON.parse(userData );
+
+
+// Index route
 app.get('/', (req, res) => {
-    // render the index view and pass an object with a single key value pair
-    res.render('index', { title: 'Index' });
+    res.render('index', {
+      title: 'Account Summary',
+      accounts: accounts
+    });
   });
-  
+
+        // profile route
+app.get('/profile', (req, res) => {
+    res.render('profile', {
+        user: users[0]
+    });
+  });
+  // Saving route
+app.get('/savings', (req, res) => {
+    res.render('account', {
+        account: accounts.savings
+    });
+  });
+
+  // Saving route
+  app.get('/checking', (req, res) => {
+    res.render('index', {
+        account: accounts.checking
+    });
+  });
+
+    // Saving route
+app.get('/credit', (req, res) => {
+    res.render('account', {
+        account: accounts.credit
+    });
+  });
+
+
+
 
   const PORT = 3000;
 
