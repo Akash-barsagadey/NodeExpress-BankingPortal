8cbd8f25d47ddc5ad0764b04fdab859a4b9b49bd
Akash Barsagadey <Akash.Barsagadey@blueconchtech.com>
2023-03-14 12:48:27 +0530

    Completed module 3

--- src/app.js
+++ src/app.js
@@ -25,6 +25,9 @@ const userData  = fs.readFileSync('src/json/users.json', { encoding: 'utf8' });
 // Parse JSON into JavaScript object
 const users  = JSON.parse(userData );
 
+// Add this middleware to handle POST data
+app.use(express.urlencoded({ extended: true }));
+
 
 // Index route
 app.get('/', (req, res) => {
@@ -32,6 +35,54 @@ app.get('/', (req, res) => {
       title: 'Account Summary',
       accounts: accounts
     });
+  });
+
+// transfer  route
+app.get('/transfer', (req, res) => {
+    res.render('transfer ', {
+      accounts: accounts
+    });
+  });
+
+
+// transfer post  route
+app.post('/transfer', (req, res) => {
+    const { from, to, amount } = req.body;
+
+    // Convert amount to a number
+    const amountNum = parseInt(amount);
+  
+    // Calculate new balances
+    accounts[from].balance -= amountNum;
+    accounts[to].balance += amountNum;
+
+      // Save updated account data to disk
+  const accountsJSON = JSON.stringify(accounts, null, 4);
+
+  fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
+
+   // Render the transfer view with success message
+   const message = "Transfer Completed";
+   res.render('transfer', { message });
+
+  });
+
+
+// payment   route
+app.get('/payment', function(req, res) {
+    res.render('payment', { account: accounts.credit });
+  });
+
+// transfer post  route
+app.post('/payment', (req, res) => {
+    accounts.credit.balance -= parseInt(req.body.amount);
+    accounts.credit.available += parseInt(req.body.amount);
+  
+    const accountsJSON = JSON.stringify(accounts);
+    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
+  
+    res.render('payment', { message: 'Payment Successful', account: accounts.credit });
+
   });
 
         // profile route
